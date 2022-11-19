83f5c4f79323758d4050aa081d0c766c02774bf0
jsonX <949463763@qq.com>
2022-11-19 15:16:15 +0800

    config

---
+++ src/components/childDemo1.vue
@@ -0,0 +1,32 @@
+<template>
+  <div style="border: solid red 1px">
+    <!-- <h1>childDemo1</h1> -->
+    {{ obj }}
+  </div>
+</template>
+<script>
+export default {
+  props: { type: { type: String } },
+  data() {
+    return {
+      data: { basic: "111", business: "222" }
+    };
+  },
+
+  computed: {
+    obj() {
+      return this.data[this.type];
+    }
+  },
+  created() {
+    console.log("created", this.obj);
+  },
+  updated() {
+    console.log("updated", this.obj);
+  },
+  destroyed() {
+    console.log("destroyed");
+  }
+};
+</script>
+<style scoped lang="less"></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,41 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <el-button type="primary" @click="click">test</el-button>
+    <el-button type="text" @click="dialogVisible = true">点击打开 Dialog</el-button>
+
+    <el-dialog title="提示" v-if="dialogVisible" :visible.sync="dialogVisible" width="30%">
+      <ChildDemo1 type="basic" />
+      <ChildDemo1 type="business" />
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
+import ChildDemo1 from "@/components/childDemo1.vue";
 
 export default {
   name: "HomeView",
   components: {
-    HelloWorld,
+    // HelloWorld,
+    ChildDemo1
   },
+  data() {
+    return {
+      dialogVisible: false
+    };
+  },
+  methods: {
+    click() {
+      console.log(1);
+    }
+  }
 };
 </script>
